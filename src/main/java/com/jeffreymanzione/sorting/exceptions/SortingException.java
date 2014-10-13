b14ee6708a25dd94022b3dc673f03884b290898e
Jeff <Jeff@LERPTERP>
2014-10-13 01:55:02 -0400

    Added a top-level exception class

---
+++ src/main/java/com/jeffreymanzione/sorting/exceptions/SortingException.java
@@ -0,0 +1,18 @@
+package com.jeffreymanzione.sorting.exceptions;
+
+/**
+ * The top-level Sorting Exception
+ * 
+ * @author Jeffrey J. Manzione
+ * @version 0.1
+ * @since October 13, 2014
+ *
+ */
+public class SortingException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -286857679833457946L;
+
+}
